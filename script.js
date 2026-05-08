093e56086bd58d8f2c9806a7d38da286ffe11e52
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-05-08 22:58:35 +0200

    fixed script.js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://j11rlr6ue3ulpk0bfrddot0q.178.105.39.91.sslip.io//";
+const API_URL = "http://j11rlr6ue3ulpk0bfrddot0q.178.105.39.91.sslip.io";
 
 async function loadQuote() {
   try {
